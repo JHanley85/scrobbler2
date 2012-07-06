35b706f10d1a93cbea2e385261bba457c92fd35e
Jack <jack@caged-media.com>
2012-07-05 20:46:39 -0700

    Added error for invalid query

--- lib/scrobbler2/geo.rb
+++ lib/scrobbler2/geo.rb
@@ -2,11 +2,15 @@ module Scrobbler2
 	class Geo < Base
 
 		def initialize(location,options={})
+			acceptable_options=['long','lat','location','distance','page','tag','festivalsonly','limit','country','metro','start','end']
 			@query={ :location => location }
 			options.each_pair do |option,value|
+				unless acceptable_options.include?(option)
+					raise "#{query} is an unacceptable option for Geo"
+				end
 			@query[option]=value
 			end
-			@query
+
 		end
 
 		has_resource :events, :root => "events"
