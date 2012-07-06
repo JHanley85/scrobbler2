e4ca353826d6cd465c48c199c7b3038e717b64cd
Jack <jack@caged-media.com>
2012-07-05 18:13:19 -0700

    Added options to Geo.new

--- lib/scrobbler2/geo.rb
+++ lib/scrobbler2/geo.rb
@@ -1,15 +1,19 @@
 module Scrobbler2
-  class Geo < Base
-    
-    def initialize(location)
-      @query = {:location=>location}#TODO eg. {:artist => artist, :track => track}
-    end
-    
-     has_resource :events, :root => "events"
-   
-     has_resource :top_artists, :root => "topartists"
-   
-     has_resource :top_tracks, :root => "toptracks"
-   
-  end
+	class Geo < Base
+
+		def initialize(location,options={})
+			@query={ :location => location }
+			options.each_pair do |option,value|
+			@query[option]=value
+			end
+			@query
+		end
+
+		has_resource :events, :root => "events"
+
+		has_resource :top_artists, :root => "topartists"
+
+		has_resource :top_tracks, :root => "toptracks"
+
+	end
 end
