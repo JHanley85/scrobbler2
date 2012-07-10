7e85146476e8efe3ead952e7fbf0234d6fd9a562
JHanley85 <jack@caged-media.com>
2012-07-10 03:19:22 -0700

    Venue and User Test return for OpenStruct

--- spec/user/user.rb
+++ spec/user/user.rb
@@ -1,2 +1,60 @@
 require_relative '../spec_helper.rb'
 $: << File.dirname(__FILE__) + "/../../../lib/"
+describe "Get information for user 'SJPollux' " do
+  before(:all) do
+    wait 30 do
+      @user=Scrobbler2::User.new(:user=>'SJPollux')
+      @artisttracks=@user.artist_tracks
+      @bannedtracks=@user.banned_tracks
+      @events=@user.events
+      @friends=@user.friends
+      @info=@user.info
+      @lovedtracks=@user.loved_tracks
+      @neighbors=@user.neighbors
+      @newreleases=@user.new_releases
+      @pastevents=@user.past_events
+      @personaltags=@user.personal_tags
+      @playlists=@user.playlists
+      @recentstations=@user.recent_stations
+      @recenttracks=@user.recent_tracks
+      @recommendedartists=@user.recommended_artists
+      @recommendedevents=@user.recommended_events
+      @shouts=@user.shouts
+      @topalbums=@user.top_albums
+      @topartists=@user.top_artists
+      @toptags=@user.top_tags
+      @toptracks=@user.top_tracks
+      @weeklyalbumchart=@user.weekly_album_chart
+      @weeklyartistchart=@user.weekly_artist_chart
+      @weeklychartlist=@user.weekly_chart_list
+      @weeklytrackchart=@user.weekly_track_chart
+    end
+  end
+  it "Should return the following as OpenStruct" do
+    @artisttracks.should be_a_kind_of OpenStruct
+    @bannedtracks.should be_a_kind_of OpenStruct
+    @events.should be_a_kind_of OpenStruct
+    @friends.should be_a_kind_of OpenStruct
+    @info.should be_a_kind_of OpenStruct
+    @lovedtracks.should be_a_kind_of OpenStruct
+    @neighbors.should be_a_kind_of OpenStruct
+    @newreleases.should be_a_kind_of OpenStruct
+    @pastevents.should be_a_kind_of OpenStruct
+    @personaltags.should be_a_kind_of OpenStruct
+    @playlists.should be_a_kind_of OpenStruct
+    @recentstations.should be_a_kind_of OpenStruct
+    @recenttracks.should be_a_kind_of OpenStruct
+    @recommendedartists.should be_a_kind_of OpenStruct
+    @recommendedevents.should be_a_kind_of OpenStruct
+    @shouts.should be_a_kind_of OpenStruct
+    @topalbums.should be_a_kind_of OpenStruct
+    @topartists.should be_a_kind_of OpenStruct
+    @toptags.should be_a_kind_of OpenStruct
+    @toptracks.should be_a_kind_of OpenStruct
+    @weeklyalbumchart.should be_a_kind_of OpenStruct
+    @weeklyartistchart.should be_a_kind_of OpenStruct
+    @weeklychartlist.should be_a_kind_of OpenStruct
+    @weeklytrackchart.should be_a_kind_of OpenStruct
+  end
+
+end
--- spec/venue/venue.rb
+++ spec/venue/venue.rb
@@ -1,2 +1,17 @@
 require_relative '../spec_helper.rb'
 $: << File.dirname(__FILE__) + "/../../../lib/"
+
+describe "Get information for ____" do
+  before(:all) do
+    wait 30 do
+      @venue=Scrobbler2::Venue.new(VENUEID)
+      @events=@venue.events
+      @pastevents= @venue.past_events
+    end
+  end
+  it "Should have the following as OpenStructs" do
+    @events.should be_a_kind_of OpenStruct
+    @pastevents.should be_a_kind_of OpenStruct
+  end
+
+end
