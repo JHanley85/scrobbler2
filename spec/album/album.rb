df537f165da3e40ebebdd35f2ce5dfc7ec9918cd
JHanley85 <jack@caged-media.com>
2012-07-10 04:25:07 -0700

    Album test stubs for each has_resource expectation

--- spec/album/album.rb
+++ spec/album/album.rb
@@ -1,11 +1,10 @@
 require_relative '../spec_helper.rb'
 $: << File.dirname(__FILE__) + "/../../../lib/"
 
-describe "album in
-formation for 'The Day I Turned To Glass' by 'Honeycut'." do
+describe "album information for 'The Day I Turned To Glass' by 'Honeycut'." do
   before(:all) do
     wait 30 do
-      @album=Scrobbler2::Album.new({:artist=>'Honeycut',:album=>'The Day I Turned To Glass'})
+      @album=Scrobbler2::Album.new({:artist => 'Honeycut', :album => 'The Day I Turned To Glass'})
       @info=@album.info
       @buylinks=@album.buylinks
       @shouts=@album.shouts
@@ -24,5 +23,35 @@ formation for 'The Day I Turned To Glass' by 'Honeycut'." do
     #pending
   end
 
+end
 
+describe "Album" do
+  before(:each) do
+    @album = Scrobbler2::Album.new(:artist => "Honeycut", :album => "The Day I Turned To Glass")
+    @album.class.stub!(:get).and_return Hash.new
+  end
+
+  it "should have a default query" do
+    @album.instance_variable_get(:@query).should =={:artist => "Honeycut", :album => "The Day I Turned To Glass"}
+  end
+  it "info should call get with :artist, :album" do
+    @album.class.should_receive(:get).with('album.getinfo', :artist => "Honeycut", :album => "The Day I Turned To Glass")
+    @album.info
+  end
+  it "buylinks should call get with :artist, :album" do
+    @album.class.should_receive(:get).with('album.getbuylinks', :artist => "Honeycut", :album => "The Day I Turned To Glass")
+    @album.buylinks
+  end
+  it "shouts should call get with :artist, :album" do
+    @album.class.should_receive(:get).with('album.getshouts', :artist => "Honeycut", :album => "The Day I Turned To Glass")
+    @album.shouts
+  end
+  it "tags should call get with :artist, :album" do
+    @album.class.should_receive(:get).with('album.gettags', :artist => "Honeycut", :album => "The Day I Turned To Glass")
+    @album.tags
+  end
+  it "top tags should call get with :artist, :album" do
+    @album.class.should_receive(:get).with('album.gettoptags', :artist => "Honeycut", :album => "The Day I Turned To Glass")
+    @album.top_tags
+  end
 end
