2d655504feb1dbb14cc2d0560480be04f77f6dd1
JHanley85 <jack@caged-media.com>
2012-07-10 14:32:08 -0700

    Album calls and methods.

--- lib/scrobbler2/album.rb
+++ lib/scrobbler2/album.rb
@@ -3,12 +3,12 @@ module Scrobbler2
     
     def initialize(options)
       acceptable_options=[:artist,:album]
-      if options.present? && options.kind_of?(Hash)
+      if options.kind_of?(Hash) && options[:artist].present? && options[:album].present?
         options.each_pair do |option, value|
           unless acceptable_options.include?(option)
             raise "#{option} is an unacceptable option for Geo"
           end
-          @query[option]=value
+          @query=options
         end
       else
         raise "You must provide a Hash with at minimum :artist, and :album."
@@ -18,8 +18,37 @@ module Scrobbler2
      has_resource :info, :root => 'album'
 	   has_resource :buylinks, :root=>'affiliations'
 	   has_resource :shouts, :root=>'shouts'
-	   has_resource :tags, :root=>'tags'
+	   has_resource :tags, :root=>'tags',:auth=>true
 	   has_resource :top_tags, :root=>'toptags'
 
+
+    def add_tags(tags)
+      unless tags.kind_of?(String)
+        raise "Tags must be string, comma delineated list. maxiumum 10 tags."
+      end
+      tag_array=tags.split(',')
+      tags=tag_array.count>10 ? tag_array.first(10).join(",") : tag_array.join(',')
+      artist=self.info.artist
+      album=self.info.name
+      query={:artist=>artist,:album=>album,:tags=>tags}
+      self.class.post_with_auth("album.addtags", query)
+    end
+    def search(query, options={ })
+      self.class.get('album.search', query, options)
+    end
+
+    def share(artist, album, recipient, options={ })
+      recipient.join(',') if recipient.kind_of?(Array)
+      query = @query.merge(:artist => artist, :album => album, :recipient => recipient)
+      query = @query.merge options if options.present?
+      self.class.post_with_auth('album.share', query)
+    end
+    def remove_tag(tag)
+      artist=self.info.artist
+      album=self.info.name
+      tag=tag.split(",").first.to_s
+      query={:artist=>artist,:album=>album,:tag=>tag}
+      self.class.post_with_auth("album.removetag", query)
+    end
   end
 end
--- spec/album/call.rb
+++ spec/album/call.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper.rb'
 $: << File.dirname(__FILE__) + "/../../../lib/"
 describe "album, non-auth" do
   before(:each) do
-    @album = Scrobbler2::Album.new(:album=>"The Day I Turned To Glass",:artist => "Honeycut")
+    @album = Scrobbler2::Album.new({:album=>"The Day I Turned To Glass",:artist => "Honeycut"})
     @album.class.stub!(:get).and_return Hash.new
   end
   it "should have a default query" do
@@ -12,46 +12,18 @@ describe "album, non-auth" do
     @album.class.should_receive(:get).with('album.getinfo',{:album =>"The Day I Turned To Glass",:artist => "Honeycut"},{})
     @album.info
   end
-  it "correction should call get with :album" do
-    @album.class.should_receive(:get).with('album.getcorrection', {:album =>"The Day I Turned To Glass",:artist => "Honeycut"},{})
-    @album.correction
-  end
-  it "events should call get with :album" do
-    @albumclass.should_receive(:get).with('artist.getevents', {:album=>"The Day I Turned To Glass",:artist => "Honeycut"},{})
-    @artist.events
-  end
-  it "past events should call get with :album" do
-    @artist.class.should_receive(:get).with('album.getpastevents', {:artist =>"The Day I Turned To Glass",:artist => "Honeycut"},{})
-    @albumpast_events
-  end
   it "shouts should call get with :artist" do
-    @album.class.should_receive(:get).with('album.getshouts',{:artist =>"The Day I Turned To Glass",:artist => "Honeycut"},{})
+    @album.class.should_receive(:get).with('album.getshouts',{:album =>"The Day I Turned To Glass",:artist => "Honeycut"},{})
     @album.shouts
-  end
-  it "similar should call get with :album" do
-    @album.class.should_receive(:get).with('album.getsimilar', {:album =>"The Day I Turned To Glass",:artist => "Honeycut"},{})
-    @album.similar
-  end
-  it "top_albums should call get with :album" do
-    @album.class.should_receive(:get).with('album.gettopalbums', {:album =>"The Day I Turned To Glass",:artist => "Honeycut"},{})
-    @album.top_albums
-  end
-  it "top_fans should call get with :album" do
-    @artist.class.should_receive(:get).with('album.gettopfans', {:album =>"The Day I Turned To Glass",:artist => "Honeycut"},{})
-    @album.top_fans
   end
   it "top_tags should call get with :album" do
     @album.class.should_receive(:get).with('album.gettoptags', {:album =>"The Day I Turned To Glass",:artist => "Honeycut"},{})
     @album.top_tags
   end
-  it "top_tracks should call get with :album" do
-    @album.class.should_receive(:get).with('album.gettoptracks', {:album =>"The Day I Turned To Glass",:artist => "Honeycut"},{})
-    @album.top_tracks
-  end
 end
 describe "album auth" do
   before(:each) do
-    @album = Scrobbler2::album.new(:album =>"The Day I Turned To Glass",:artist => "Honeycut")
+    @album = Scrobbler2::Album.new(:album =>"The Day I Turned To Glass",:artist => "Honeycut")
     @album.class.stub!(:get_with_auth).and_return Hash.new
   end
   it "tags should call get_with_auth with :album" do
@@ -61,9 +33,9 @@ describe "album auth" do
 end
 describe "album Add/Remove Tags" do
   before(:each) do
-    @album = Scrobbler2::Album.new(:album => "Honeycut")
+    @album = Scrobbler2::Album.new(:album => "Honeycut",:artist => "Honeycut")
     @album.class.stub!(:post_with_auth).and_return Hash.new
-    @album.stub!(:info).and_return(OpenStruct.new({:name=>'Honeycut'}))
+    @album.stub!(:info).and_return(OpenStruct.new({:name=>'The Day I Turned To Glass',:artist=>'Honeycut'}))
   end
   it "add_tags should call post_with_auth with :album and :tags" do
     @album.class.should_receive(:post_with_auth).with('album.addtags',{:album=>"The Day I Turned To Glass",:artist =>'Honeycut',:tags=>'Electronic'})
@@ -75,5 +47,6 @@ describe "album Add/Remove Tags" do
   end
 
 end
-describe "Album sharing of Honeycut's The Day I turned to Glass, with '"
+describe "Album sharing of Honeycut's The Day I turned to Glass, with '" do
+end
 
