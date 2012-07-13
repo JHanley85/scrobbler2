56399ec8ef7060c0d2a6211353d4911735fa7829
JHanley85 <jack@caged-media.com>
2012-07-13 05:33:43 -0700

    Moved api keys/secret/token to config file. Added tag post test for Album.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'httparty'
 require 'fakeweb'
+require_relative '../config/scrobbler.rb'
 require_relative '../lib/scrobbler2.rb'
 $: << File.dirname(__FILE__) + "/../lib/"
 
@@ -20,9 +21,7 @@ end
 # These are real, so that the tests will pass.
 # The session-key allows authenticated access as the last.fm user 'scrobbler2'
 # Please do not use them in your own apps.
-Scrobbler2::Base.api_key = "c62652cbaabd91e0553c7b415c5a3dbc"
-Scrobbler2::Base.api_secret = "03c60c8b07bab6f8e91675d8e16fcd0c"
-Scrobbler2::Base.session_key = "91518cf316876a405f3f8194724a3c6e"
+# Moved all configuration to config/scrobbler2.rb
 
 def wait(time, increment = 5, elapsed_time = 0, &block)
 	begin
@@ -36,4 +35,3 @@ def wait(time, increment = 5, elapsed_time = 0, &block)
 		end
 	end
 end
-
